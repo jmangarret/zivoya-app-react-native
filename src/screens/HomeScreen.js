033b59c2b5e4fb350743cf44a0d6e280d26cb995
Jonathan Mangarret <jmangarret@gmail.com>
2024-08-05 11:02:38 -0500

    fix libraries working

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Home Screen 1111</Text>
     </View>
   );
 }
