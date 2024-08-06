e0d3321ed86ee59cbc8e311182ceb239b8b9050f
Jonathan Mangarret <jmangarret@gmail.com>
2024-08-06 11:47:31 -0500

    add assets and icons

---
+++ assets/colors/colors.js
@@ -0,0 +1,39 @@
+// Color combination
+
+// Orange buttons backgrounds and fonts
+const PRIMARY_01 = '#F16D2B';
+// Dark backgrounds and fonts
+const SECONDARY_01 = '#2A3946';
+// Type of seats
+const OPTIONAL_01 = '#2775B7';
+// Disabled
+const DISABLED_01 = '#C9C9C9';
+
+// Gray colors
+const GRAY_01 = '#88929A';
+const GRAY_02 = '#F5F5F5';
+const GRAY_03 = '#FFFFFF';
+// Range of background colors
+const DEGRADE_ORANGE_01 = '#F16D2B - #F15A5A';
+
+export const PRIMARY = PRIMARY_01;
+export const SECONDARY = SECONDARY_01;
+export const OPTIONAL = OPTIONAL_01;
+export const DISABLED = DISABLED_01;
+export const GRAY01 = GRAY_01;
+export const GRAY02 = GRAY_02;
+export const GRAY03 = GRAY_03;
+export const DEGRADE_ORANGE = DEGRADE_ORANGE_01;
+
+export const THEME = {
+  colors: {
+    primary: PRIMARY,
+    accent: SECONDARY,
+  },
+  fonts: {
+    regular: 'Roboto-Regular',
+    medium: 'Roboto-Bold',
+    light: 'Robot-Light',
+    thin: 'Roboto-Thin',
+  },
+};
