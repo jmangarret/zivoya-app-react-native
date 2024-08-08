22ee486286f5c7570999761a7f54a4127cf5fc1e
Jonathan Mangarret <jmangarret@gmail.com>
2024-08-08 11:37:08 -0500

    fix screen

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import FlightSearch from './src/components/FlightSearch';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
-import { applyMiddleware, createStore, legacy_createStore } from 'redux';
-import {thunk} from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
+import FlightSearch from './src/components/FlightSearch';
+
+const Stack = createNativeStackNavigator();
 export default class App extends Component {
   store = createStore(applyMiddleware(thunk));
 
   render() {
     return (
       <Provider store={store}>
-        <FlightSearch />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={FlightSearch}  options={{ title: 'Zivoya' }}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
     );
   }
